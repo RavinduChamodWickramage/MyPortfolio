2443cf7474246a4f023a8e20dcc5159ffd0d038e
Ravindu Chamod Wickramage <ravinduchamod1@gmail.com>
2025-03-03 00:04:19 +0530

    Update cv link

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "Passionate Software Developer dedicated to creating impactful solutions and enhancing user experiences. Committed to innovation and continuous skill growth in software engineering. Seeking aninternship to contribute to meaningful projects and expand my expertise.",
   github: "https://github.com/RavinduChamodWickramage",
   resume:
-    "https://drive.google.com/file/d/18XBlZho5BotiA5dZQ4xHotmrckd4qhPm/view?usp=drive_link",
+    "https://drive.google.com/file/d/1KL-LOFTEYYJDYKWn5FlvKKmGkBMx5_IL/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ravindu-chamod-wickramage/",
   insta: "https://www.instagram.com/official_mr_wicky/",
   facebook: "https://www.facebook.com/ravindu.chamod.wickramage",
